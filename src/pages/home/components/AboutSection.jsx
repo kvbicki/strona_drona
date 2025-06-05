80e98bbd1ddf132683151458227fde173d947f35
skubicki342 <szymon.kubicki342@gmail.com>
2025-06-05 19:41:43 +0200

    Dodanie Aurora, poprawa kolorow

--- src/pages/home/components/AboutSection.jsx
+++ src/pages/home/components/AboutSection.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { motion } from "framer-motion";
 import { team } from "../util/mock.js"
 import AboutSectionList from "./AboutSectionList.jsx";
+import "../styles/about.css";
 
 
 
--- src/pages/home/components/DronesSection.jsx
+++ src/pages/home/components/DronesSection.jsx
@@ -2,12 +2,21 @@ import React from "react";
 import DronesSectionList from "./DronesSectionList.jsx";
 import { drones } from "../util/mock.js";
 import "../styles/drones.css";
+import Aurora from "./Aurora.jsx"
+
 
 const DronesSection = () => (
+  <> <Aurora
+  colorStops={["#83A8D3", "#83A8D3", "#83A8D3"]}
+  blend={0.05}
+  amplitude={1.0}
+  speed={0.5}
+/>
   <section id="drones" className="drones-section">
     <h2>Nasza oferta</h2>
     <DronesSectionList drones={drones} />
   </section>
+  </>
 );
 
 export default DronesSection;
